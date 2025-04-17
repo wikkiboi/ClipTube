21b68d17a3bb0c5a1b0347391224b9ecb02318eb
wikkiboi <mofouyu@gmail.com>
2025-04-17 01:36:49 -0700

    Webhook setup + database sync w/app

---
+++ src/app/api/users/webhook/route.ts
@@ -0,0 +1,51 @@
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { db } from "../../../../db";
+import { users } from "../../../../db/schema";
+import { eq } from "drizzle-orm";
+
+export async function POST(req: Request) {
+  try {
+    const evt = await verifyWebhook(req);
+
+    // Do something with payload
+    // For this guide, log payload to console
+    const eventType = evt.type;
+
+    if (eventType === "user.created") {
+      const { data } = evt;
+
+      await db.insert(users).values({
+        clerkId: data.id,
+        name: `${data.first_name} ${data.last_name}`,
+        imageUrl: data.image_url,
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      const { data } = evt;
+
+      if (!data.id) {
+        return new Response("Missing user id", { status: 400 });
+      }
+
+      await db.delete(users).where(eq(users.clerkId, data.id));
+    }
+
+    if (eventType === "user.updated") {
+      const { data } = evt;
+
+      await db
+        .update(users)
+        .set({
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url,
+        })
+        .where(eq(users.clerkId, data.id));
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
