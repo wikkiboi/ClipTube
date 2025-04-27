17a2b9670d7d5d3d08fba1b8068b82a9a8847366
wikkiboi <mofouyu@gmail.com>
2025-04-27 02:10:00 -0700

    Infinite scroll component for studio

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,10 +1,32 @@
 "use client";
 
+import { Suspense } from "react";
 import { DEFAULT_LIMIT } from "../../../../constants";
 import { trpc } from "../../../../trpc/client";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "../../../../components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../../../components/ui/table";
+import { useRouter } from "next/navigation";
 
 export const VideosSection = () => {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  return (
+    <Suspense fallback={<p>Loading..</p>}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -12,5 +34,49 @@ export const VideosSection = () => {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     }
   );
-  return <div>{JSON.stringify(data)}</div>;
+
+  const router = useRouter();
+
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <TableRow
+                  key={video.id}
+                  className="cursor-pointer"
+                  onClick={() => router.push(`/studio/videos/${video.id}`)}
+                >
+                  <TableCell>{video.title}</TableCell>
+                  <TableCell>visibility</TableCell>
+                  <TableCell>status</TableCell>
+                  <TableCell>comments</TableCell>
+                  <TableCell>likes</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
 };
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,8 +1,6 @@
 import { db } from "../../../db";
 import { videos } from "../../../db/schema";
 import { createTRPCRouter, protectedProcedure } from "../../../trpc/init";
-import { z } from "zod";
-import { eq, and, or, lt, desc } from "drizzle-orm";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
